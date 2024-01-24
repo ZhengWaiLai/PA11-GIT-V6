8f96be4d998bf6ca57e792dcae1f6014e95f7af8
ZhengWaiLai <laizhengwai05@gmail.com>
2024-01-24 11:50:27 +0800

    Updated game

---
+++ 38_ZhengWai_Lab10/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject1;
+    public GameObject SpawnObject2;
+    public GameObject SpawnObject3;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObject1, transform.position, transform.rotation);
+
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObject2, transform.position, transform.rotation);
+
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObject3, transform.position, transform.rotation);
+    }
+}
